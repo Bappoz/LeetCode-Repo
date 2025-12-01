89025b8ee2f5a57170d669498885e1f9472a927a
Bappoz <landradezanetti@gmail.com>
2025-11-30 21:26:10 -0300

    Resolved: 142. Linked List Cycle II

---
+++ C/2-Medium/linked_cycle_II/linked_list_cycle_II.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct ListNode {
+    int val;
+    struct ListNode *next;
+};
+
+
+struct ListNode *detectCycle(struct ListNode *head) {
+    struct ListNode* slow = head;
+    struct ListNode* fast = head;
+    
+    while(fast && fast->next){
+        slow = slow->next;
+        fast = fast->next->next;
+        if(slow == fast) break;
+    }
+
+    if(!fast|| !fast->next) return NULL;
+
+
+    slow = head;
+    while(slow != fast){
+        slow = slow->next;
+        fast = fast->next;
+    }
+    return slow;
+}
